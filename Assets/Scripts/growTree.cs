72e3782d305356ec85f70e4c65f0222c215ca072
Joan Martí Olivart <jmartiolivart@gmail.com>
2023-02-04 14:26:54 +0100

    Tree growing up
    Hello

--- Assets/Scripts/growTree.cs
+++ Assets/Scripts/growTree.cs
@@ -7,20 +7,26 @@ public class growTree : MonoBehaviour
     public SpriteRenderer spriteRenderer;
     public Sprite newSprite;
     public Sprite[] spriteArray;
+    public int actualSprite = 0;
     // Start is called before the first frame update
-    void Start()
+   void Start()
     {
-         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("HIIIIIIIIIII");
+            ChangeSprite();
+        }
     }
 
     void ChangeSprite()
     {
-        spriteRenderer.sprite = newSprite; 
+        actualSprite++;
+        spriteRenderer.sprite = spriteArray[actualSprite]; 
     }
 }
